d68f54267890526a9763b596ca0c1b9de891ba3e
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2019-10-15 07:27:46 +0200

    Squale specific headers.

---
+++ toolchain/include/squale_hw.h
@@ -0,0 +1,17 @@
+// Apollo Squale memory mapping
+// (c) 2016-2019 HxC2001 / Jean-François DEL NERO
+
+#define HW_RAM_BASE               0x0000
+#define HW_EF9365_BASE            0xF000
+#define HW_CTRL_REG_BASE          0xF010
+#define HW_VIDMEM_RD1_BASE        0xF020
+#define HW_VIDMEM_RD3_BASE        0xF030
+#define HW_KEYBOARD_EF6821_BASE   0xF044
+#define HW_CARTRIDGE_EF6821_BASE  0xF048
+#define HW_MODEM_TAPE_EF6850_BASE 0xF050
+#define HW_SOUND_AY_3_8910A_BASE  0xF060
+#define HW_EXTENSION_BASE         0xF080
+#define HW_ROM_BASE               0xF100
+
+#define HW_RAM_SIZE               0xF000
+#define HW_EXTENSION_SIZE         0x0080
---
+++ toolchain/include/stdint.h
@@ -0,0 +1,5 @@
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
+
+typedef short int16_t;
+typedef unsigned short uint16_t;
