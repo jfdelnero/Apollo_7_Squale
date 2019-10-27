a48e10d8542b2f13e3227fd9055e4fa56a60b3f7
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2019-10-27 11:20:52 +0100

    music build process changed.

--- examples/Squale_3D_demo/ym/packed_ym.h
+++
@@ -1,4 +0,0 @@
-extern const unsigned char * ym_reg_blockmaps[14];
-extern const unsigned char * ym_reg_blocks[14];
-#define YM_PAGES_NUMBERS 462
-#define YM_PAGES_SIZE 16
--- examples/Squale_3D_demo/ym_music.h
+++
@@ -1,2 +0,0 @@
-extern const unsigned char ymmusic[25690];
-
