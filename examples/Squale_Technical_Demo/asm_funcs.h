6f67d2b15b4074a6551b2a16eb30747f0e9b7837
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2019-11-02 01:19:06 +0100

    vid_asm_func removed.

--- examples/Squale_Technical_Demo/asm_funcs.h
+++ examples/Squale_Technical_Demo/asm_funcs.h
@@ -24,5 +24,4 @@
 // Change History (most recent first):
 ///////////////////////////////////////////////////////////////////////////////////
 
-void vid_asm_func(unsigned char value);
 void progymregs_asm_func(unsigned char *regs);
